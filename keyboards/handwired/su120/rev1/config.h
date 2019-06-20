4b21cccd82c93dfd85dfa37533438827684de8dc
e3w2q <50942793+e3w2q@users.noreply.github.com>
2019-06-20 23:18:47 +0900

    Update PID

--- keyboards/handwired/su120/rev1/config.h
+++ keyboards/handwired/su120/rev1/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x1209
-#define PRODUCT_ID      0x0001
+#define PRODUCT_ID      0x4649
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    e3w2q
 #define PRODUCT         SU120
