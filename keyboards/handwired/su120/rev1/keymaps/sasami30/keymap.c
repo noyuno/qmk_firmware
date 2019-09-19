2e3f4238d279d419244845e9f2f11dbf396884b7
e3w2q <50942793+e3w2q@users.noreply.github.com>
2019-09-19 21:56:14 +0900

    Update

--- keyboards/handwired/su120/rev1/keymaps/sasami30/keymap.c
+++ keyboards/handwired/su120/rev1/keymaps/sasami30/keymap.c
@@ -21,15 +21,62 @@ enum custom_keycodes {
   SEND_000
 };
 
+#define LCTL_Z   LCTL_T(KC_Z)
+#define LSFT_X   LSFT_T(KC_X)
+#define LALT_C   LALT_T(KC_C)
+#define LGUI_V   LGUI_T(KC_V)
+#define LT1_BSC  LT(1, KC_BSPC)
+#define LT2_SPC  LT(2, KC_SPC)
+#define RGUI_B   RGUI_T(KC_B)
+#define RALT_N   RALT_T(KC_N)
+#define RCTL_M   RCTL_T(KC_M)
+#define RSFT_ENT RSFT_T(KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Default Layer
+  /* Layer 0 (Default QWERTY)
+   *  Master Side (USB Cable Connected Side)
+   *  ,---------------------------------------------------
+   *  |  XXXX  XXXX  XXXX  XXXX   XXXX    XXXX  XXXX  XXXX  XXXX    XXXX 
+   *  |     Q     W     E     R      T       Y     U     I     O       P 
+   *  |     A     S     D     F      G       H     J     K     L     Del 
+   *  | Ctl/Z Sft/X Alt/C GUI/V LT1/BS LT2/Spc GUI/B Alt/N Ctl/M Sft/Ent 
+   *  |  XXXX  XXXX  XXXX  XXXX   XXXX    XXXX  XXXX  XXXX  XXXX    XXXX 
+   *  |  XXXX  XXXX  XXXX  XXXX   XXXX    XXXX  XXXX  XXXX  XXXX    XXXX 
+   *  `---------------------------------------------------
+   *  Slave Side (USB Cable Not Connected Side)
+   *  ,---------------------------------------------------
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  `---------------------------------------------------
+   */
+  [0] = LAYOUT( /* Base */ 
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     
+    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_DEL,   
+    LCTL_Z,   LSFT_X,   LALT_C,   LGUI_V,   LT1_BSC,  LT2_SPC,  RGUI_B,   RALT_N,   RCTL_M,   RSFT_ENT, 
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX   
+  ),
+
+  /* Layer 1 (Symbol)
    *  Master Side (USB Cable Connected Side)
    *  ,---------------------------------------------------
    *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
-   *  |    Q    W    E    R    T    Y    U    I    O    P 
-   *  |    A    S    D    F    G    H    J    K    L   BS 
-   *  |    Z    W    C    V    B    N    M Spce Entr MO(1)
+   *  |    1    2    3    4    5    6    7    8    9    0 
+   *  |   F1   F2   F3   F4   F5   F6   F7   F8   F9  F10 
+   *  | Ctl+ Sft+ Alt+ GUI+ ____ PtSc    ,    .  F11  F12 
    *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
    *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
    *  `---------------------------------------------------
@@ -43,12 +90,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
    *  `---------------------------------------------------
    */
-
-  [0] = LAYOUT( /* Base */ 
+  /* Layer 2 (Number)
+   *  Master Side (USB Cable Connected Side)
+   *  ,---------------------------------------------------
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  |    1    2    3    4    5    6    7    8    9    0 
+   *  |   F1   F2   F3   F4   F5   F6   F7   F8   F9  F10 
+   *  | Ctl+ Sft+ Alt+ GUI+ PtSc ____    ,    .  F11  F12 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  `---------------------------------------------------
+   *  Slave Side (USB Cable Not Connected Side)
+   *  ,---------------------------------------------------
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  | XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX 
+   *  `---------------------------------------------------
+   */
+  [1] = LAYOUT(
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
-    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     
-    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_BSPC,  
-    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_SPC,   KC_ENT,   MO(1),    
+    RGB_TOG,  RGB_MOD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
+    RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  EEP_RST,  
+    RGB_HUD,  RGB_SAD,  RGB_VAD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,    
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
 
@@ -59,8 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX   
   ),
-
-  [1] = LAYOUT(
+  [2] = LAYOUT(
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
     RGB_TOG,  RGB_MOD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
     RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  EEP_RST,  
